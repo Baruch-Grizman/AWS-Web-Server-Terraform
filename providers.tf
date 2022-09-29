d1b298322d9af93168990c325b1c35c41868dc78
Baruch-Grizman <103598882+Baruch-Grizman@users.noreply.github.com>
2022-09-29 17:07:00 +0300

    testing providers.tf without provider "aws" block

--- providers.tf
+++ providers.tf
@@ -18,8 +18,8 @@ terraform {
   }
 }
 
-provider "aws" {
-  shared_config_files      = ["~/.aws/config"]
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "default"
-}
+#provider "aws" {
+#  shared_config_files      = ["~/.aws/config"]
+#  shared_credentials_files = ["~/.aws/credentials"]
+#  profile                  = "default"
+#}
